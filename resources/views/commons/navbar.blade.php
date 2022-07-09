6f06fcde8ccee5e776222b8a6a5c0b9a286d8495
kazu.kirameki <transfer01@gairai.sakura.ne.jp>
2022-07-09 01:41:34 +0000

    navbar.blade edit

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -17,10 +17,12 @@
                         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
                         <ul class="dropdown-menu dropdown-menu-right">
                             {{-- ユーザ詳細ページへのリンク --}}
-                            <li class="dropdown-item">{!! link_to_route('users.show', 'My profile', ['user' => Auth::id()]) !!}</li>
+                            <!-- li class="dropdown-item">{!! link_to_route('users.show', 'My profile', ['user' => Auth::id()]) !!}</li -->
+                            <li class="dropdown-item">{!! link_to_route('users.show', 'My profile', [Auth::id()]) !!}</li>
                             <li class="dropdown-divider"></li>
                             {{-- お気に入り一覧ページへのリンク --}}
-                            <li class="dropdown-item">{!! link_to_route('users.favorites', 'Favorites', [Auth::user()->id]) !!}</li>
+                            <!-- li class="dropdown-item">{!! link_to_route('users.favorites', 'Favorites', [Auth::user()->id]) !!}</li -->
+                            <li class="dropdown-item">{!! link_to_route('users.favorites', 'Favorites', [Auth::id()]) !!}</li>
                             <li class="dropdown-divider"></li>
                             {{-- ログアウトへのリンク --}}
                             <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
